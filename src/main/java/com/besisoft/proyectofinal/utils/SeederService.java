8fe7e9de4eba750ec5cf42c2128d84e65270a2d7
SofiRoadToDev <sofi.workaffairs@gmail.com>
2023-03-20 21:42:52 -0300

    Carga de un vehiculo y un cliente desvinculados como seed

---
+++ src/main/java/com/besisoft/proyectofinal/utils/SeederService.java
@@ -0,0 +1,47 @@
+package com.besisoft.proyectofinal.utils;
+
+import com.besisoft.proyectofinal.entity.Cliente;
+import com.besisoft.proyectofinal.entity.Vehiculo;
+import com.besisoft.proyectofinal.repository.ClienteRepository;
+import com.besisoft.proyectofinal.repository.VehiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SeederService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Autowired
+    VehiculoRepository vehiculoRepository;
+
+
+    @EventListener
+    public void seed(ContextRefreshedEvent event){
+        Cliente cliente=Cliente.builder()
+                .id(1L)
+                .correoElectronico("cliente@gmail.com")
+                .telefonoLinea("123456789321456")
+                .nombres("Alejandro")
+                .apellido("Figueroa")
+                .build();
+        clienteRepository.save(cliente);
+
+        Vehiculo vehiculo=Vehiculo.builder()
+                .id(2L)
+                .marca("chevrolet")
+                .modelo("corsa")
+                .anio(2015)
+                .patente("123abcd")
+                .color("azul")
+                .build();
+        vehiculoRepository.save(vehiculo);
+    }
+
+
+
+
+}
